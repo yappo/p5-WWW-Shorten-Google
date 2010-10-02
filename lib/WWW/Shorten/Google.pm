ed5aeda18659f15289c2e6f1c73b995044187b8d
sunnavy <sunnavy@bestpractical.com>
2010-10-02 14:08:32 +0800

    goo.gl has a public page now

--- lib/WWW/Shorten/Google.pm
+++ lib/WWW/Shorten/Google.pm
@@ -7,109 +7,28 @@ our @EXPORT = qw( makeashorterlink makealongerlink );
 our $VERSION = '0.01';
 
 use Carp;
-use POSIX 'floor';
-use JSON::Any;
-
-sub _c {
-    my $l = 0;
-    for my $val (@_) {
-        $val &= 4294967295;
-        # 32bit signed
-        $val += $val > 2147483647 ? -4294967296 :
-                    $val < -2147483647 ? 4294967296 : 0;
-        $l += $val;
-        # 32bit signed
-        $l += $l > 2147483647 ? -4294967296 :
-                  $l < -2147483647 ? 4294967296 : 0;
-    }
-    return $l;
-}
-
-sub _e {
-    my $uri = shift;
-
-    my $m   = 5381;
-    for my $char (split '', $uri) {
-        $m = _c( $m << 5, $m, unpack('C', $char) );
-    }
-    return $m;
-}
-
-sub _f {
-    my $uri = shift;
-
-    my $m   = 0;
-    for my $char (split '', $uri) {
-        $m = _c( unpack('C', $char), $m << 6, $m << 16, -$m );
-    }
-    return $m;
-}
-
-sub _d {
-    my $l = shift;
-    $l = ( $l > 0 ) ? $l : $l + 4294967296;
-
-    my $m = "$l";
-    my $n = 0;
-    my $o = 0;
-    for my $char (reverse split '', $m) {
-        my $q = $char + 0;
-        $o += $n ? ($q *= 2, floor($q / 10) + $q % 10) : $q;
-        $n = !$n;
-    }
-
-    $m = $o % 10;
-    $o = 0;
-
-    if ($m != 0) {
-        $o = 10 - $m;
-        if (length($l) % 2 == 1) {
-            $o += 9 if $o % 2 == 1;
-            $o /= 2;
-        }
-    }
-
-    return "$o$l";
-}
-
-
-sub _generate_auth_token {
-    my $uri = shift;
-
-    my $i = _e($uri);
-    $i = $i >> 2 & 1073741823;
-    $i = $i >> 4 &   67108800 | $i &    63;
-    $i = $i >> 4 &    4193280 | $i &  1023;
-    $i = $i >> 4 &     245760 | $i & 16383;
-
-    my $h = _f($uri);
-
-    my $k = ($i >>  2 & 15) <<  4 | $h & 15;
-    $k   |= ($i >>  6 & 15) << 12 | ($h >>  8 & 15) <<  8;
-    $k   |= ($i >> 10 & 15) << 20 | ($h >> 16 & 15) << 16;
-    $k   |= ($i >> 14 & 15) << 28 | ($h >> 24 & 15) << 24;
-    my $j = "7" . _d($k);
-}
 
 sub makeashorterlink {
     my $uri = shift or croak 'No URL passed to makeashorterlink';
-    my $user = shift || 'toolbar@google.com';
-
-    my $token = _generate_auth_token $uri;
-
-    my $ua = __PACKAGE__->ua();
+    my $ua  = __PACKAGE__->ua();
     my $res = $ua->post(
-        'http://goo.gl/api/url' => [
-            user       => $user,
-            url        => $uri,
-            auth_token => $token,
-        ]
+        'http://goo.gl/action/shorten',
+        {
+            url    => $uri,
+            authed => 1,
+        }
     );
-    return unless $res->is_success;
 
-    my $json = JSON::Any->jsonToObj( $res->content );
-    return unless $json->{short_url};
-    return $json->{short_url};
+    if ( $res->is_redirect ) {
+        return $1 if $res->header('location') =~ /url=(.*)/;
+    }
+    elsif ( $res->is_success ) {
+        if ( $res->content =~ /id="result".*?value="(.*?)"/i ) {
+            return $1;
+        }
+    }
+
+    return;
 }
 
 sub makealongerlink {
@@ -123,8 +42,6 @@ sub makealongerlink {
     return unless $res->is_redirect;
     return $res->header('Location');
 }
-
-
 
 1;
 __END__
@@ -140,8 +57,6 @@ WWW::Shorten::Google -  Perl interface to goo.gl
 
   $short_url = makeashorterlink($long_url);
   $long_url  = makealongerlink($short_url);
-
-  $short_url = makeashorterlink($long_url, 'YOUR goo.gl USER NAME');
 
 =head1 DESCRIPTION
 
@@ -167,7 +82,7 @@ Kazuhiro Osawa E<lt>yappo <at> shibuya <döt> plE<gt>
 
 =head1 SEE ALSO
 
-L<WWW::Shorten>, L<http://goo.gl/>, my idea takes by L<http://www.kix.in/blog/2009/12/goo-gl/>
+L<WWW::Shorten>, L<http://goo.gl/>
 
 =head1 LICENSE
 
